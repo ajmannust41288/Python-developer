5fde11989b552be353a2af85d8485e4231f72d54
ajmannust41288 <124865655+ajmannust41288@users.noreply.github.com>
2023-07-18 23:39:57 +0500

    Dice game
    Dice rolling projects

---
+++ dice.py
@@ -0,0 +1,14 @@
+# ====================================Rolling a dice game
+# import random
+#
+#
+# def roll_dice():
+#     num_dice = int(input("Enter the number of dice to roll: "))
+#     num_sides = int(input("Enter the number of sides on each die: "))
+#
+#     for _ in range(num_dice):
+#         result = random.randint(1, num_sides)
+#         print("Roll:", result)
+#
+#
+# roll_dice()
